b940c0737f167e27bd7aa59d4178b3cd7d7deb1b
Ilyasse Idkhebbach <iidkhebb@e1r3p12.1337.ma>
2022-04-05 22:11:47 +0000

    added ex01 in CPP00

---
+++ CPP00/ex01/Phonebook.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <iostream>
+#define print(s) std::cout << s
+
+class PhoneBook
+{
+private:
+    char contacts[8];
+public:
+};
--- CPP00/ex01/setup.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <iostream>
-#include <string>
-
-#define print(s) std::cout << s
