d8f679c4bb010188d1ad4e4d2f562b462c44bb1d
iidkhebb <iidkhebb@student.1337.ma>
2022-06-05 07:55:02 +0100

    added CPP03

---
+++ CPP03/ex01/main.cpp
@@ -0,0 +1,13 @@
+#include "ScavTrap.hpp"
+#include "ClapTrap.hpp"
+
+int main()
+{
+	// ScavTrap scav("robot");
+	// scav.attack("enemy_1");
+	// scav.guardGate();
+	// scav.takeDamage(3);
+	// scav.beRepaired(2);
+	// scav = ScavTrap("anOther");
+	return 0;
+}
