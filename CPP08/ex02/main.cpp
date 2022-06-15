15841a6948433aa0d1cac338cb69cf24b2ec5d77
Ilyasse Idkhebbach <iidkhebb@e3r9p8.1337.ma>
2022-06-15 20:25:22 +0100

    added ex02 in CPP08

--- CPP08/ex02/main.cpp
+++ CPP08/ex02/main.cpp
@@ -5,23 +5,28 @@ int main()
     MutantStack<int> mstack;
     mstack.push(5);
     mstack.push(17);
-    std::cout << mstack.top() << std::endl;
-    mstack.pop();
-    std::cout << mstack.size() << std::endl;
-    mstack.push(3);
-    mstack.push(5);
-    mstack.push(737);
-    //[...]
-    mstack.push(0);
-    MutantStack<int>::iterator it = mstack.begin();
-    MutantStack<int>::iterator ite = mstack.end();
-    ++it;
-    --it;
+    
+    std::cout << "------------------- \n" <<  " value at the top of the stack -> "<< mstack.top() << "\n-------------------\n" << std::endl;
+    mstack.pop(); // <- remove the top element
+    std::cout << "-------------------- \n" << "The size of the stack -> " << mstack.size() << "\n-------------------\n" <<  std::endl;
+    
+    mstack.push(3); // <- add an element to the top of the stack
+    mstack.push(5); // <- add an element to the top of the stack
+    mstack.push(737); // <- add an element to the top of the stack
+    mstack.push(0); // <- add an element to the top of the stack
+
+    MutantStack<int>::iterator it = mstack.begin(); // <- get the iterator on the first element of the stack
+    MutantStack<int>::iterator ite = mstack.end(); // <- get the iterator on the last element of the stack
+   
+    ++it; // <- increment the iterator
+    --it; // <- decrement the iterator
+    std::cout << "--------------------" << "The value of the iterator " << "-------------------" <<  std::endl;
     while (it != ite)
     {
-        std::cout << *it << std::endl;
+        std::cout << *it << std::endl; // <- print the values
         ++it;
     }
-    std::stack<int> s(mstack);
+
+    std::stack<int> s(mstack); // <- copy the MutantStack to a std::stack
     return (0);
 }
